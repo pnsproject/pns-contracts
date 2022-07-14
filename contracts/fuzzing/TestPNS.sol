e449a53f976311ba45c4ac494494d40e55915f36
tester <tester@example.com>
2022-07-14 21:35:52 +0800

    TestPNS: add op for transferRootOwnership & setManager
    syntax ok, not test yet.

--- contracts/fuzzing/TestPNS.sol
+++ contracts/fuzzing/TestPNS.sol
@@ -8,6 +8,9 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+import "../pns/PNS.sol";
+import "../pns/PNSController.sol";
+
 import "./EchidnaInit.sol";
 import "./IHEVM.sol";
 import "./OpenForwarder.sol";
@@ -55,5 +58,193 @@ contract TestPNS is EchidnaInit, OpenForwarder {
     bool[2] _c_is_open    = [true, true];
     bool[2] _c_can_redeem = [true, true];
 
-    // ----------------------
+    // ------------------------- helper function -------------------
+    function h_sender(uint8 idx) internal view returns(address) {
+        return SENDER_POOL.at(idx % SENDER_POOL.length());
+    }
+
+    function h_sender_with_c(uint8 idx) internal view returns(address) {
+        uint len = SENDER_POOL.length();
+        idx = uint8(idx % (2 + len));
+
+        if (idx >= len) {
+            return address(C[idx - len]);
+        }
+        else {
+            return SENDER_POOL.at(idx);
+        }
+    }
+
+    function h_call(address c, bytes memory d) internal returns(bool, bytes memory) {
+        return execute(msg.sender, c, d);
+    }
+
+    function h_call_assert(bool ok, address c, bytes memory d) internal returns(bytes memory) {
+        (bool ok_, bytes memory r) = h_call(c, d);
+        assert(ok_ == ok);
+        return r;
+    }
+
+    function h_p_call(bytes memory d) internal returns(bool, bytes memory) {
+        return h_call(address(P), d);
+    }
+
+    function h_p_call_assert(bool ok, bytes memory d) internal returns(bytes memory) {
+        return h_call_assert(ok, address(P), d);
+    }
+
+    function h_c_call(uint idx, bytes memory d) internal returns(bool, bytes memory) {
+        return h_call(address(C[idx]), d);
+    }
+
+    function h_c_call_assert(bool ok, uint idx, bytes memory d) internal returns(bytes memory) {
+        return h_call_assert(ok, address(C[idx]), d);
+    }
+
+    // ---------------------- operation ---------------------------
+    function op_p_transferRootOwnership(uint8 fix_r, address p_r) public {
+        // requirements
+
+        // param generation
+        if (fix_r < 250) {
+            p_r = h_sender(fix_r);
+        }
+        else {
+            fix_r = 0; // dummy
+        }
+
+        // update state
+        bool ok = false;
+
+        if (msg.sender == _pns_root) {
+            ok = true;
+        }
+
+        if (ok) {
+            _pns_root = p_r;
+        }
+
+        // call op
+        h_p_call_assert(ok, abi.encodeWithSelector(P.transferRootOwnership.selector, p_r));
+
+        if (!ok) {
+            return;
+        }
+
+        // assertion
+        assert(P.root() == p_r);
+    }
+
+    function op_c_transferRootOwnership(uint8 idx, uint8 fix_r, address p_r) public {
+        // requirements
+
+        // param generation
+        if (fix_r < 250) {
+            p_r = h_sender(fix_r);
+        }
+        else {
+            fix_r = 0;
+        }
+
+        uint8 p_idx = idx % 2;
+
+        // update state
+        bool ok = false;
+
+        if (msg.sender == _c_root[p_idx]) {
+            ok = true;
+        }
+
+        if (ok) {
+            _c_root[p_idx] = p_r;
+        }
+
+        // call op
+        h_c_call_assert(ok, p_idx, abi.encodeWithSelector(C[p_idx].transferRootOwnership.selector, p_r));
+
+        if (!ok) {
+            return;
+        }
+
+        // assertion
+        assert(C[p_idx].root() == p_r);
+    }
+
+    function op_p_setManager(uint8 fix_m, bool p_b) public {
+        // requirements
+
+        // param generation
+        address p_m = h_sender_with_c(fix_m);
+
+        // update state
+        bool ok = false;
+
+        if (msg.sender == _pns_root) {
+            ok = true;
+        }
+
+        if (ok) {
+            if (p_b) {
+                _pns_manager_set.add(p_m);
+            }
+            else {
+                _pns_manager_set.remove(p_m);
+            }
+        }
+
+        // call op
+        h_p_call_assert(ok, abi.encodeWithSelector(P.setManager.selector, p_m, p_b));
+
+        if (!ok) {
+            return;
+        }
+
+        // assertion
+        assert(P.isManager(p_m) == p_b);
+    }
+
+    function op_c_setManager(uint8 idx, uint8 fix_m, bool p_b) public {
+        // requirements
+
+        // param generation
+        uint8 p_idx = idx % 2;
+        address p_m = h_sender(fix_m);
+
+        // update state
+        bool ok = false;
+
+        if (msg.sender == _c_root[p_idx]) {
+            ok = true;
+        }
+
+        if (ok) {
+            if (p_b) {
+                _c_manager_set[p_idx].add(p_m);
+            }
+            else {
+                _c_manager_set[p_idx].remove(p_m);
+            }
+        }
+
+        // call op
+        h_c_call_assert(ok, p_idx, abi.encodeWithSelector(C[p_idx].setManager.selector, p_m, p_b));
+
+        if (!ok) {
+            return;
+        }
+
+        // assertion
+        assert(C[p_idx].isManager(p_m) == p_b);
+    }
+
+    // requirements
+    // param generation
+    // update state
+    // call op
+    // assertion
+
+    // ----------------------- partial operation --------------------
+
+    // ----------------------- aux operation ------------------------
+
 }
